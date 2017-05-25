2c25545935435f54e7983e79e90aee89eb52a89e
zzhang115 <zzhang115@dons.usfca.edu>
2017-05-24 22:59:17 -0700

    add apend string to a existing file

--- emplyeeInfoTable.py
+++ emplyeeInfoTable.py
@@ -4,6 +4,9 @@ while len(line) != 0:
     print line
     line = f.readline()
 f.close()
-w = file("result", "w")
-w.write("hello boy, finish reading!")
-w.close()
+# w = file("result", "w")
+# w.write("hello boy, finish reading!")
+# w.write("Hello, I am zzc ")
+with open("result", "a") as w: #this is for writing string appends to existing file
+    w.write("Hello")
+    w.close()

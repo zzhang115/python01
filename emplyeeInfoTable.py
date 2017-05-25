70222a21ede162c312eaa68150fd3a6deb31f1e0
zzhang115 <zzhang115@dons.usfca.edu>
2017-05-24 23:52:49 -0700

    add replace string in a file and change some line in a file

--- emplyeeInfoTable.py
+++ emplyeeInfoTable.py
@@ -1,12 +1,24 @@
+import fileinput
 f = file("EmployeeInfo")
 line = f.readline()
 while len(line) != 0:
     print line
     line = f.readline()
+    print f.tell()
 f.close()
-# w = file("result", "w")
-# w.write("hello boy, finish reading!")
-# w.write("Hello, I am zzc ")
-with open("result", "a") as w: #this is for writing string appends to existing file
+w = file("result", "w")
+w.write("hello boy, finish reading!")
+w.write("Hello, I am zzc ")
+w.close()
+
+with open("result", "a") as w: #this is for writing string appends to existing file a represent append
     w.write("Hello")
     w.close()
+for line in fileinput.input("result", inplace=0):
+    line = line.replace("Hi", "Hello")
+    print "line:", line #it mean get everything from result file and after replacing, it will write line back
+
+with open("result", "r+") as f:
+    old = f.read() #read everything in the file
+    f.seek(0) #rewind it means set the file's current position to 0
+    f.write("new line\n" + old) #write the new

ab8f28c9ca23333c9ffea4d7186d45d7e240cb9f
zzhang115 <zzhang115@dons.usfca.edu>
2017-05-25 22:14:12 -0700

    python list

--- emplyeeInfoTable.py
+++ emplyeeInfoTable.py
@@ -1,11 +1,11 @@
 import fileinput
 f = file("EmployeeInfo")
-line = f.readline()
-while len(line) != 0:
-    print line
-    line = f.readline()
-    print f.tell()
-f.close()
+# line = f.readline()
+# while len(line) != 0:
+#     print line
+#     line = f.readline()
+#     print f.tell()
+# f.close()
 w = file("result", "w")
 w.write("hello boy, finish reading!")
 w.write("Hello, I am zzc ")
@@ -14,14 +14,15 @@ w.close()
 with open("result", "a") as w: #this is for writing string appends to existing file a represent append
     w.write("Hello")
     w.close()
+
 for line in fileinput.input("result", inplace=0): #replace string in file
     line = line.replace("Hi", "Hello")
     print "line:", line #it mean get everything from result file and after replacing, it will write line back
 
-with open("result", "r+") as f:
+with open("result", "r+") as f:#r+ means it can be changed
     old = f.read() #read everything in the file
     f.seek(10) #rewind it means set the file's current position to 10,
-    f.write("new line\n" + old) #write the new add new line string after cursor position 10
+    f.write("new line\n" ) #write the new add new line string after cursor position 10
 w = file("backupfile", "w")
 w.write("hello boy, this is test file")
 w.close()
@@ -30,3 +31,12 @@ for line in fileinput.input("backupfile", backup=".bak", inplace=1):#for this fu
     line = line.replace("hello", "hi")  #and back up one file end with .bak, it wont be replace by hi
     print line
 
+# with open("EmployeeInfo", "r") as f:
+#     newText=f.read().replace("9", "6")
+#     print newText
+# with open("EmployeeInfo", "w") as f:
+#      f.write(newText)
+
+for line in fileinput.input("EmployeeInfo", inplace=1):
+    line = line.replace("759", "656")
+    print line
---
+++ list.py
@@ -0,0 +1,5 @@
+nameList = ["abc", "def", "ghi"]
+print nameList[2]
+# nameList.pop(1)
+# nameList.remove("abc")
+print nameList

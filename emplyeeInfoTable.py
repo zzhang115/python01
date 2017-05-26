bad2f399226c9e30d35da833b1389343f94bd0a4
zzhang115 <zzhang115@dons.usfca.edu>
2017-05-25 20:59:23 -0700

    add backup function

--- emplyeeInfoTable.py
+++ emplyeeInfoTable.py
@@ -14,7 +14,7 @@ w.close()
 with open("result", "a") as w: #this is for writing string appends to existing file a represent append
     w.write("Hello")
     w.close()
-for line in fileinput.input("result", inplace=0):
+for line in fileinput.input("result", inplace=0): #replace string in file
     line = line.replace("Hi", "Hello")
     print "line:", line #it mean get everything from result file and after replacing, it will write line back
 
@@ -22,3 +22,11 @@ with open("result", "r+") as f:
     old = f.read() #read everything in the file
     f.seek(10) #rewind it means set the file's current position to 10,
     f.write("new line\n" + old) #write the new add new line string after cursor position 10
+w = file("backupfile", "w")
+w.write("hello boy, this is test file")
+w.close()
+
+for line in fileinput.input("backupfile", backup=".bak", inplace=1):#for this function, it change the content of backupfile
+    line = line.replace("hello", "hi")  #and back up one file end with .bak, it wont be replace by hi
+    print line
+

ded4ba9d75d1d4a4bcea30388f2de515a2d03cc6
zzhang115 <zzhang115@dons.usfca.edu>
2017-05-26 21:08:51 -0700

    dictionary test

---
+++ dict.py
@@ -0,0 +1,31 @@
+DicName = {
+   "01" : "boy",
+   "02" : "girl"
+}
+Dic = {
+   01 : "boy",
+   02 : "girl"
+}
+print DicName.get("01")
+print Dic.get(2)
+print DicName["02"]
+print DicName.has_key("01")
+print DicName.keys()
+print DicName.items()
+
+for i in DicName.keys():
+   print i, "price:", DicName.get(i)
+
+DicName.popitem()
+
+for i in DicName.keys():
+   print i, "secprice:", DicName.get(i)
+
+DicName["03"] = "transfer gender"
+for i in DicName.keys():
+   print i, "thirdprice:", DicName.get(i)
+
+DicName.pop("01")
+for i in DicName.keys():
+   print i, "fourthprice:", DicName.get(i)
+

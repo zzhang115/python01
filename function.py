ff682c97001e54619363f9ed1807ceb83ebd16c0
zzhang115 <zzhang115@dons.usfca.edu>
2017-05-26 22:47:07 -0700

    add new manual(mode of openning file)

---
+++ function.py
@@ -0,0 +1,18 @@
+def sayHello():
+    print "hello world"
+
+def saySth(name):
+    print "hello baby %s" %name
+
+sayHello()
+saySth("zzc")
+def name_info(name, age, job, nationnality="Chinese"):
+    global gl, gl1
+    gl = 123
+    gl1 = 100
+    print "your name: %s age %d your nationnality:%s" %(name, age, nationnality)
+    return name
+result = name_info("zzc", 23, "programmar", )
+print result
+print gl, gl1
+

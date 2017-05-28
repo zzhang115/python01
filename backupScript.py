8c1a6c472ba1b577d529f6d0fac6c21fd919ed0b
zzhang115 <zzhang115@dons.usfca.edu>
2017-05-27 23:18:47 -0700

    add backupfile

--- backupScript.py
+++ backupScript.py
@@ -5,7 +5,9 @@ path = sys.argv[1]
 filename = path.split("/")[-1]
 
 backup_directory = "/NewSpace/Python/PythonWorkplace/backupFile/"
+backup_filename = '''%s%s_%s.tgz''' %(backup_directory, filename, time.strftime("%H:%M:%S_%m-%d-%y", time.localtime()))
 
+def backupFile()
 # def recordLogFile(backupDate, status, files, description = 'NULL'):
 #     date = time.strftime("%H:%M:%S %m-%d-%y")
 #     recordContent = "%s\t%s\t%s\t%s\n" %(date, backupDate, status, files, description)

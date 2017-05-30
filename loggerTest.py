20415d10be6beabb68633742a5c9acbf79c9790b
zzhang115 <zzhang115@dons.usfca.edu>
2017-05-29 19:47:54 -0700

    add logging test and .log example file

---
+++ loggerTest.py
@@ -0,0 +1,5 @@
+import logging
+logging.basicConfig(filename="example.log", level=logging.DEBUG)
+logging.debug("this message for debug")
+logging.warning("Watch out")
+logging.info("this is logging info")

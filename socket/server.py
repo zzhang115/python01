c17fa9fa4a480c75327dd210c1570a262ab23396
zzhang115 <zzhang115@dons.usfca.edu>
2017-06-02 21:03:08 -0700

    add socket

--- socket/server.py
+++ socket/server.py
@@ -1,5 +1,18 @@
 import socket
 
-HOST = '' #represent default
-PORT = 8080
+HOST = '0.0.0.0'#'0.0.0.0' #represent default
+PORT = 9999
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP
+s.bind((HOST, PORT))
+s.listen(2)
+conn, addr = s.accept()
+
+print 'GOT connection from:', addr
+
+while 1:
+    data = conn.recv(4096)#receive 4kb character once
+    if not data:
+        break
+    conn.sendall(data)
+    print data
+conn.close()

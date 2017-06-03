8100fb39e05b52fd0bfbb3637f76d41d12d264b2
zzhang115 <zzhang115@dons.usfca.edu>
2017-06-03 10:41:02 -0700

    client send message continually

--- socket/client.py
+++ socket/client.py
@@ -1,11 +1,13 @@
-import socket
+import socket, time
 
 HOST = '127.0.0.1'#'localhost' #'0.0.0.0'#'0.0.0.0' #represent default
 PORT = 9999
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP
 s.connect((HOST, PORT))
 
-s.sendall("first message from client")
-data = s.recv(1024)
+while 1:
+    s.sendall("first message from client1")
+    data = s.recv(1024)
+    time.sleep(1)
+    print "receive message from server", repr(data)
 s.close()
-print "receive message from server", repr(data)
--- socket/server.py
+++ socket/server.py
@@ -1,4 +1,4 @@
-import socket
+import socket, time
 
 HOST = 'localhost'#'0.0.0.0'#'0.0.0.0' #represent default
 PORT = 9999
@@ -11,9 +11,11 @@ s.listen(2)
 while 1:
     conn, addr = s.accept()
     print 'GOT connection from:', addr
-    data = conn.recv(1024)#receive 4kb character once
-    if not data:
-        continue
-    conn.sendall(data)
-    print data
+    while 1:
+        data = conn.recv(1024)#receive 4kb character once
+        if not data:
+            continue
+        conn.sendall(data)
+        print data
+        time.sleep(1)
 conn.close()
